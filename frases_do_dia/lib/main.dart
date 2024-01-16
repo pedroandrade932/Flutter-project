f61500ca398417626e666ba8fe9f534afaaa7136
pedroandrade932 <andraderpedro@gmail.com>
2024-01-16 13:29:47 -0300

    ss

--- frases_do_dia/lib/main.dart
+++ frases_do_dia/lib/main.dart
@@ -1,75 +1,37 @@
-import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(
+    home: Home(),
+    debugShowCheckedModeBanner: false,
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Home extends StatefulWidget {
+  const Home({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme:
-            ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 255, 0, 0)),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Frases do dia.'),
-    );
-  }
+  State<Home> createState() => _HomeState();
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _numAleatorio = 0;
-  final List _frases = [
-    "O importante não é vencer todos os dias, mas lutar sempre.",
-    "É melhor conquistar a si mesmo do que vencer mil batalhas.",
-    "O medo de perder tira a vontade de ganhar",
-    "Perder para a razão, sempre é ganhar."
-  ];
-
-  void _incrementCounter() {
-    setState(() {
-      _numAleatorio = Random().nextInt(4);
-    });
-  }
-
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: Text("Frases do dia"),
+        backgroundColor: Colors.green,
       ),
-      body: Center(
+      body: Container(
+        padding: EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          border: Border.all(width: 3, color: Colors.amber)
+        ),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Pressione o botão para gerar uma frase:',
-            ),
-            Text(
-              _frases[_numAleatorio],
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+          children: [
+            Image.asset("images/logo.png")
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.wordpress),
       ),
     );
   }
